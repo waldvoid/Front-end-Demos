7b9016ad5b5fdbcc26e7cc61f149963f696bd0e8
Mert Evirgen <sokko4123@gmail.com>
2023-03-27 00:14:45 +0300

    Ping Coming Soon Page

---
+++ Ping Coming Soon Page/README-template.md
@@ -0,0 +1,28 @@
+# Ping Coming Soon Page
+
+This is results summary component made with HTML, CSS & Javascript.
+
+## Table of contents
+
+- [Overview](#overview)
+    - [Links](#links)
+    - [Built with](#built-with)
+    - [Screenshot](#screenshot)
+
+### Links
+
+- Repository URL: [Github Repository](https://github.com/waldvoid/Front-end-Demos/tree/main/Interactive%20Rating%20Component)
+- Live Site URL: [Live Page](https://pingpage-merte.netlify.app)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript
+
+### Screenshots
+<div style="display: flex; flex-wrap: nowrap; width: 100%; justify-content: center; align-items: center">
+<img src="design/desktop.png" style="max-width: 500px; height: 100%;">
+<img src="design/mobile.png" style="max-width: 200px; height: 100%;" >
+</div>
---
+++ Ping Coming Soon Page/script/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const emailValidation = document.getElementById("email-input");
+    const emailInvalidError = document.getElementById("email-invalid");
+
+    // opacity & margin manage event listener
+    // check input validation
+    emailValidation.addEventListener("input", function () {
+        if (emailValidation.checkValidity()) {
+            emailInvalidError.style.opacity = "1";
+            emailInvalidError.style.margin = ".5em 0 1.5em 0";
+        } else {
+            emailInvalidError.style.opacity = "0";
+            emailInvalidError.style.margin = "0";
+        }
+    });
+
+    // check user focus on input
+    emailValidation.addEventListener("blur", function () {
+        if (!emailValidation.checkValidity()) {
+            emailInvalidError.style.opacity = "1";
+            emailInvalidError.style.margin= ".5em 0 1.5em 0";
+        } else {
+            emailInvalidError.style.opacity = "0";
+            emailInvalidError.style.margin = "0";
+        }
+    });
+    emailInvalidError.style.transition = "all 0.5s ease-in-out";
+});
