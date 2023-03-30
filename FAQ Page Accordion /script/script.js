cb0d21970f0f7dffbcfd451d5a1dd22170e5661c
Mert Evirgen <95648576+waldvoid@users.noreply.github.com>
2023-03-30 11:12:37 +0300

    Delete FAQ Page Accordion  directory

--- FAQ Page Accordion /script/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const accordionItems = document.querySelectorAll(".accordion-item");
-
-    accordionItems.forEach(function (item) {
-        item.addEventListener("click", function () {
-            item.classList.toggle("rotate");
-        })
-    })
-})
