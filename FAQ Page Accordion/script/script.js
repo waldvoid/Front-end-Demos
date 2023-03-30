9839b7d6340e8cd31af30f6f31535a98534dff0e
waldvoid <95648576+waldvoid@users.noreply.github.com>
2023-03-30 17:01:04 +0300

    Accordion - Open / Close

--- FAQ Page Accordion/script/script.js
+++ FAQ Page Accordion/script/script.js
@@ -1,9 +1,35 @@
+let isAnyAccordionOpen = false;
 document.addEventListener("DOMContentLoaded", function () {
     const accordionItems = document.querySelectorAll(".accordion-item");
 
     accordionItems.forEach(function (item) {
         item.addEventListener("click", function () {
-            item.classList.toggle("rotate");
+            const accordionText = item.querySelector(".accordion-text");
+            if(item.classList.contains("rotate")) {
+                // time interval for transition into accordion-item::after
+                setTimeout(function() {
+                    item.classList.remove("rotate");
+                    accordionText.style.width = "0";
+                    accordionText.style.height = "0";
+                    accordionText.style.overflow = "hidden";
+                    accordionText.style.paddingTop = "0";
+                }, 100);
+                isAnyAccordionOpen = false;
+            } else {
+                accordionItems.forEach(function (item){
+                    item.classList.remove("rotate");
+                    accordionText.style.width = "0";
+                    accordionText.style.height = "0";
+                    accordionText.style.overflow = "hidden";
+                    accordionText.style.paddingTop = "0";
+                })
+            }
+            item.classList.add("rotate");
+            accordionText.style.width = "100%";
+            accordionText.style.height = "100%";
+            accordionText.style.paddingTop = "2em";
+            accordionText.style.overflow = "visible";
+            isAnyAccordionOpen = true;
+            })
         })
     })
-})
