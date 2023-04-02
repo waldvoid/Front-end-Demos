38f038b05829d0e2538c807f44480e233e3a613f
Mert Evirgen <sokko4123@gmail.com>
2023-04-02 14:29:56 +0300

    code arrangement improvements

--- Article Preview Component/script/script.js
+++ Article Preview Component/script/script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const shareButton =  document.getElementById("share-button");
-    const sharePanel = document.getElementById("share-panel");
+	const shareButton = document.getElementById("share-button");
+	const sharePanel = document.getElementById("share-panel");
 
-    shareButton.addEventListener("click", function () {
+	shareButton.addEventListener("click", function () {
+		setTimeout(function () {
+			if (!sharePanel.classList.contains("visible")) {
+				sharePanel.classList.add("visible");
+				sharePanel.ariaHidden = "false";
+				sharePanel.tabIndex = 0;
 
-        setTimeout(function () {
-            if (!sharePanel.classList.contains("visible")) {
-                sharePanel.classList.add("visible");
-                sharePanel.ariaHidden = "false";
-                sharePanel.tabIndex = 0;
-
-            } else if (sharePanel.classList.contains("visible")){
-                sharePanel.classList.remove("visible");
-                sharePanel.ariaHidden = "true";
-                sharePanel.tabIndex = -1;
-            }
-        }, 100);
-    })
+			} else if (sharePanel.classList.contains("visible")) {
+				sharePanel.classList.remove("visible");
+				sharePanel.ariaHidden = "true";
+				sharePanel.tabIndex = -1;
+			}
+		}, 100);
+	})
 })
