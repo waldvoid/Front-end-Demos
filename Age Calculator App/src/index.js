5cf08f21d59703752fe4386e5823ee434e056063
Mert Evirgen <sokko4123@gmail.com>
2023-04-09 20:18:00 +0300

    Calculation formula optimization

--- Age Calculator App/src/index.js
+++ Age Calculator App/src/index.js
@@ -65,18 +65,46 @@ function calculateResults(dayInput, monthInput, yearInput) {
 	// safari compatibility, ISO format transformation with padStart
 	const personalDateObject = new Date(`${Number(yearInput.value)}-${Number(monthInput.value).toString().padStart(2, "0")}-${Number(dayInput.value).toString().padStart(2, "0")}`);
 	const currentDateObject = new Date();
+
+	// optimal solution
 	const diffInAbs = Math.abs(currentDateObject - personalDateObject);
 	const totalDiffDays = Math.floor(diffInAbs / (1000 * 60 * 60 * 24));
-	const diffInYears = Math.floor(totalDiffDays / 365);
-	const diffInMonths = Math.floor((totalDiffDays % 365) / 30);
-	const diffInDays = ((totalDiffDays % 365) % 30);
+	const monthDaysAverageIn4Years = 30.4375;
+	const diffInYearsNoRounding = ( totalDiffDays / (4 * 365 + 1) ) * 4;
+	const diffInYears = Math.floor(diffInYearsNoRounding);
+	const diffInMonthsNoRounding = (diffInYearsNoRounding - diffInYears) * 12;
+	const diffInMonths = Math.floor((diffInYearsNoRounding - diffInYears) * 12);
+	const diffInDays = Math.round((diffInMonthsNoRounding - diffInMonths) * monthDaysAverageIn4Years);
+
+	// Less optimal solution
+	// let diffInYears, diffInMonths, diffInDays;
+	// let diffInYears = Math.floor(totalDiffDays / 365);
+	// let diffInMonths =  Math.floor((totalDiffDays % 365) / 30.43);
+	// let diffInDays = Math.floor(((totalDiffDays % 365) % 30.43));
+	// let leapDays = 0;
+	// function leapYearCounter() {
+	// 	for (let i = personalDateObject.getFullYear() ; i <= currentDateObject.getFullYear() ; i++) {
+	// 		if((i % 4 === 0 && i % 100 !== 0) || i % 400 === 0) {
+	// 			leapDays++;
+	// 		}
+	// 	}
+	// 	const leapYears = Math.floor(leapDays / 365);
+	// 	console.log(leapYears);
+	// 	const leapMonths = Math.floor((leapYears % 365) / 30);
+	// 	console.log(leapMonths);
+	// 	leapDays = (leapDays % 365) % 30;
+	// 	console.log(leapDays);
+	// 	diffInYears = Math.floor((totalDiffDays - 4) / 365);
+	// 	diffInMonths =  Math.floor(((totalDiffDays - leapDays) % 365) / 30);
+	// 	diffInDays = (((totalDiffDays - leapDays) % 365) % 30);
+	// }
+	// leapYearCounter();
 	return [diffInYears, diffInMonths, diffInDays];
 }
 
 function submitForm(event, dayInput,monthInput,yearInput) {
 	const inputFields = document.querySelectorAll("input");
 	const resultFields = document.querySelectorAll(".result-number");
-	const submitButton = document.getElementById("submit-button");
 
 	if (Array.from(inputFields).some(input => input.classList.contains("empty") || input.classList.contains("invalidDay") || input.classList.contains("invalidMonth") || input.classList.contains("invalidYear"))) {
 		resultFields[0].innerHTML = "--";
